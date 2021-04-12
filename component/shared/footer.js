c552101833bca197941fb33757ba34299bee6f52
Dewa <adewalade@gmail.com>
2021-04-12 13:24:09 +0100

    feat: add footer to website

--- component/shared/footer.js
+++ component/shared/footer.js
@@ -1,8 +1,80 @@
 import React from 'react';
 
+import Link from 'next/link';
+
+import { Row, Col } from "react-bootstrap";
+
 export default function Footer () {
-    return(
-        <>
-        </>
-        )
+    return (
+      <footer id="footer">
+        <Row id="footer-top pt-4 pb-4">
+          <Col>
+            <p className="ml-4 pl-4">Expeditemovers Ltd. is a highly dynamic logistic service provider positioned to render efficient and personalized service to her esteemed clients with 100% satisfaction guaranteed.</p>
+          </Col>
+          <Col>
+            <ul>
+              <li className="list-unstyled">
+                <Link href="/">
+                  <a className="footer-link">Home</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="/services">
+                  <a className="footer-link">Services</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="/about">
+                  <a className="footer-link">About Us</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="/contact">
+                  <a className="footer-link">Contact Us</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="/locations">
+                  <a className="footer-link">Locations</a>
+                </Link>
+              </li>
+            </ul>
+          </Col>
+          <Col>
+            <h6>Our social Media</h6>
+            <ul>
+              <li className="list-unstyled">
+                <Link href="#">
+                  <a className="footer-link">Facebook</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="#">
+                  <a className="footer-link">Twitter</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="#">
+                  <a className="footer-link">Instagram</a>
+                </Link>
+              </li>
+              <li className="list-unstyled">
+                <Link href="#">
+                  <a className="footer-link">LinkdIn</a>
+                </Link>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row id="footer-bottom">
+          <h6 className="text-center pt-2 pb-2">
+            &copy; {new Date().getFullYear()}{" "}
+            <Link href="/">
+              <a className="footer-link"> ExpediteMovers Ltd. </a>
+            </Link>
+            Made in Lagos, Nigeria
+          </h6>
+        </Row>
+      </footer>
+    );
 }
--- layout/layout.js
+++ layout/layout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 import Header from '../component/shared/header';
 import Footer from '../component/shared/footer';
 
@@ -10,28 +12,29 @@ export default function Layout ({children}) {
           <title>Home | Expedite Movers Ltd.</title>
 
           <meta charSet="utf-8" />
-          <meta name="author" content="Ade-ONojobi Oluwadewalade" />
+          <meta name="author" content="Ade-Onojobi Oluwadewalade" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
-          <meta name="description" content="Expedite movers is a Nigerian logistics service commited to providing the best in shipping and transportation services." />
+          <meta name="description" content="Expeditemovers is a Nigerian logistics service commited to providing the best in shipping and transportation services." />
 
-          <meta name="og:title" content={siteTitle} />
+          {/* <meta name="og:title" content={siteTitle} /> */}
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="" />
           <meta property="og:url" content="https://moviecritics.vercel.app/" />
           <meta property="og:image" content="https://res.cloudinary.com/hellodewa/image/upload/v1616334477/Moviecritics/images/logos/moviecritics-flavicon_se6axa.ico" />
-          <meta property="og:description" content="Expedite movers is a Nigerian logistics service commited to providing the best in shipping and transportation services." />
+          <meta property="og:description" content="Expeditemovers is a Nigerian logistics service commited to providing the best in shipping and transportation services." />
 
-          <meta property="og:site_name" content="Moviecritics" />
-          <meta name="twitter:image:alt" content="Moviecritics" />
+          <meta property="og:site_name" content="ExpediteMovers" />
+          <meta name="twitter:image:alt" content="ExpediteMovers" />
 
           <link rel="shortcut icon" href="https://res.cloudinary.com/hellodewa/image/upload/v1616334477/Moviecritics/images/logos/moviecritics-flavicon_se6axa.ico" />
           <link rel="icon" href="https://res.cloudinary.com/hellodewa/image/upload/v1616334477/Moviecritics/images/logos/moviecritics-flavicon_se6axa.ico" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"></link>
           <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <Header />
-        <main>{children}</main>
+        <main className='ml-4 pl-4 mr-4 pr-4'>{children}</main>
         <Footer />
       </>
     );
