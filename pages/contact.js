ca934229c5427a557bc91ccddadaf54fdae9738e
Dewa <adewalade@gmail.com>
2021-04-26 21:08:23 +0100

    feat: add contact form to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -4,15 +4,58 @@ import Layout from "../layout/layout";
 
 import { Row, Col } from "react-bootstrap";
 
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
 export default function Contact () {
     return (
       <Layout>
         <Row>
-          <Col className="ml-2 pl-4 mr-2 pr-4">
-            <h4>Contact Us</h4>
-            <TextField id="standard-basic" label="Standard" />
+          <Col className="ml-2 pl-4 mr-2 pr-4 pt-4 align-items-center">
+            <form className="mt-4 pt-4" id="contact-form">
+              <h4 className="mt-4 pt-2">Contact Us</h4>
+              <div className="mb-4">
+                <TextField
+                  id="name-textfield"
+                  label="name"
+                  fullWidth
+                  placeholder="Your name"
+                  variant="outlined"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+              <div className="mb-4">
+                <TextField
+                  id="email-textfield"
+                  label="email"
+                  fullWidth
+                  placeholder="Your email"
+                  variant="outlined"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+              <div className="mb-4">
+                <TextField
+                  id="message-textfield"
+                  label="message"
+                  multiline
+                  rows={5}
+                  fullWidth
+                  placeholder="Enter your message here...."
+                  variant="outlined"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+              <Button variant="contained" size="large" fullwidth disableElevation color="primary">
+                Send message
+              </Button>
+            </form>
           </Col>
           <Col>
             <img src="https://res.cloudinary.com/hellodewa/image/upload/v1619447228/expeditemovers/images/sys/contact-us-side-image_ql8vme.jpg" alt="man calling on a phone" width="100%" />
