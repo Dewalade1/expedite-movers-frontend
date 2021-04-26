58ff3f2c12df0728b88a9ce74599a9dddfe3d42f
Dewa <adewalade@gmail.com>
2021-04-26 14:54:47 +0100

    chore: minor changes to nav bar
    Aligned nav links to the right

--- component/shared/header.js
+++ component/shared/header.js
@@ -8,11 +8,11 @@ export default function Header() {
     return (
       <div className="pt-2 pb-2" id="header">
         <Row>
-          <Col>
-            <img className="pl-4 ml-4" src="https://res.cloudinary.com/hellodewa/image/upload/v1616559517/Moviecritics/images/logos/moviecritics-logo-transparent-background_sjnfhk.png" width={180} height={50} alt="sitelogo" />
+          <Col className="pl-4 ml-4">
+            <img src="https://res.cloudinary.com/hellodewa/image/upload/v1616559517/Moviecritics/images/logos/moviecritics-logo-transparent-background_sjnfhk.png" width={180} height={50} alt="sitelogo" />
           </Col>
           <Col>
-            <ul className="nav mr-sm-2">
+            <ul className="nav d-flex mr-sm-2  justify-content-end">
               <li className="nav-item">
                 <Link href="/">
                   <a className="nav-link active">Home</a>
