3d8ba0058c07474aeb85d37044fc2891a283462a
Dewa <adewalade@gmail.com>
2021-04-26 15:03:49 +0100

    chore: adjust header

--- component/shared/header.js
+++ component/shared/header.js
@@ -6,13 +6,13 @@ import { Row, Col } from "react-bootstrap";
 
 export default function Header() {
     return (
-      <div className="pt-2 pb-2" id="header">
+      <div className="pt-2 pb-4 mr-4 pr-4 pl-4" id="header">
         <Row>
           <Col className="pl-4 ml-4">
             <img src="https://res.cloudinary.com/hellodewa/image/upload/v1616559517/Moviecritics/images/logos/moviecritics-logo-transparent-background_sjnfhk.png" width={180} height={50} alt="sitelogo" />
           </Col>
-          <Col>
-            <ul className="nav d-flex mr-sm-2  justify-content-end">
+          <Col className="pr-4 mr-4">
+            <ul className="nav d-flex justify-content-end mr-4 pr-4">
               <li className="nav-item">
                 <Link href="/">
                   <a className="nav-link active">Home</a>
