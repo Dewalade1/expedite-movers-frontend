534b2f8b0efd2ebc149ad13964f31151eb45cff3
Dewa <adewalade@gmail.com>
2021-04-26 14:39:06 +0100

    refactor: move bootstrap css from layout
    Moved the css classnames in layout to individual files

--- layout/layout.js
+++ layout/layout.js
@@ -30,11 +30,13 @@ export default function Layout ({children}) {
           <link rel="icon" href="https://res.cloudinary.com/hellodewa/image/upload/v1616334477/Moviecritics/images/logos/moviecritics-flavicon_se6axa.ico" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"></link>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"/>
           <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         </Head>
         <Header />
-        <main className='ml-4 pl-4 mr-4 pr-4'>{children}</main>
+        <main>{children}</main>
         <Footer />
       </>
     );
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Layout from '../layout/layout';
 
 export default function Home() {
   return (
-    <Layout></Layout>
+    <Layout>
+      <div className="ml-4 pl-4 mr-4 pr-4">
+        Welcome to expeditemovers
+      </div>
+    </Layout>
   )
 }
