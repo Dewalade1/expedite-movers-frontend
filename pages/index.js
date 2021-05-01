5a420909c8e01422a676544cda0f456c1a5e4b89
Dewa <adewalade@gmail.com>
2021-05-01 16:53:21 +0100

    feat: made landing page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
+import { Row, Col } from "react-bootstrap";
+
 import Layout from '../layout/layout';
 
 export default function Home() {
   return (
     <Layout>
-      <div className="ml-4 pl-4 mr-4 pr-4">
-        Welcome to expeditemovers
-      </div>
+      <Row className="section-wrapper" id="landing-page">
+        <Col className="ml-4 pl-4 mr-4 pr-4 d-flex align-items-center">
+          <div className="landing-page-col-1 pl-4 ml-4">
+            <h1 id="main-landing-page-text">
+              <b>Welcome to expeditemovers</b>
+            </h1>
+            <h3 className="landing-page-text d-flex justify-content-center">We make your logistics easier for you. When working with us, peace of mind is assured.</h3>
+          </div>
+        </Col>
+        <Col className="d-flex align-items-center">
+          <img src="https://res.cloudinary.com/hellodewa/image/upload/v1619884004/expeditemovers/images/sys/landing-page-img_brmhyv.png" />
+        </Col>
+      </Row>
+      <Row className="section-wrapper"></Row>
     </Layout>
-  )
+  );
 }
