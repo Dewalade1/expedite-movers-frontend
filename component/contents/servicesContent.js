bcf1a31b96ed205ff5f8d481acfdffac589696b1
Dewa <adewalade@gmail.com>
2021-05-11 13:40:56 +0100

    feat: add layout for services page

--- component/contents/servicesContent.js
+++ component/contents/servicesContent.js
@@ -3,12 +3,56 @@ import React from 'react';
 import {Row, Col } from 'react-bootstrap';
 
 export default function ServicesContent () {
-    return(
-        <>
-            <Row className="section-wrapper">
-                <Col></Col>
-                <Col></Col>
-            </Row>
-        </>
-    )
+    return (
+      <>
+        <Row className="section-wrapper">
+          <Col></Col>
+          <Col>
+            <h2>
+              <b>Haulage</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="section-wrapper">
+          <Col>
+            <h2>
+              <b>Freight</b>
+            </h2>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row className="section-wrapper">
+          <Col></Col>
+          <Col>
+            <h2>
+              <b>Haulage</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="section-wrapper">
+          <Col>
+            <h2>
+              <b>Freight</b>
+            </h2>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row className="section-wrapper">
+          <Col></Col>
+          <Col>
+            <h2>
+              <b>Haulage</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="section-wrapper">
+          <Col>
+            <h2>
+              <b>Freight</b>
+            </h2>
+          </Col>
+          <Col></Col>
+        </Row>
+      </>
+    );
 };
