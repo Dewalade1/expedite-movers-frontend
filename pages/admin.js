74f47def642752dc841d6e1d2ddc6ad26f9df0a4
Dewa <adewalade@gmail.com>
2021-05-23 20:10:09 +0100

    feat: add email and database cards to admin page

--- pages/admin.js
+++ pages/admin.js
@@ -2,19 +2,47 @@ import React from 'react';
 
 import { Row, Col } from 'react-bootstrap';
 
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+
+import Button from "@material-ui/core/Button";
+
 import Layout from '../layout/layout';
 
 export default function Admin () {
-    return( 
-        <Layout>
-            <header>
-                Admin page
-            </header>
-            <Row>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-            </Row>
-        </Layout>
-    )
+    return (
+      <Layout>
+        <header id="admin-header">
+          <h1>Admin</h1>{" "}
+        </header>
+        <Row>
+          <Col className="admin-col-layout">
+            <Card className="admin-card" variant="outlined">
+              <CardHeader title="Databases" />
+              <CardContent>
+                View and edit your Databases Here
+                <Button variant="contained" color="primary" size="medium" disableElevation fullWidth>
+                  {" "}
+                  Go to Database{" "}
+                </Button>
+              </CardContent>
+            </Card>
+          </Col>
+          <Col className="admin-col-layout">
+            <Card className="admin-card" variant="outlined">
+              <CardHeader title="Email" />
+              <CardContent>
+                View and edit your Databases Here
+                <Button variant="contained" color="primary" size="medium" disableElevation fullWidth>
+                  {" "}
+                  Access Email{" "}
+                </Button>
+              </CardContent>
+            </Card>
+          </Col>
+        </Row>
+      </Layout>
+    );
 }
