93ced311a37db4dd9b11ede1a5b8e770c9a64586
Dewa <adewalade@gmail.com>
2021-05-28 19:45:27 +0100

    chore: make location page responsive

--- pages/about-us.js
+++ pages/about-us.js
@@ -6,13 +6,13 @@ import ClientContent from "../component/contents/clientContent";
 export default function AboutUs () {
     return (
       <Layout>
-        <header className="d-flex justify-content-center align-items-center" id="about-us-header">
+        <header className="align-items-origin" id="about-us-header">
           <div>
-            <h2 className="text-center about-us-title">
+            <h2 className="primary-title">
               {" "}
               <b> About Us </b>
             </h2>
-            <h5 className="text-center services-title">Who we are</h5>
+            <h5 className="secondary-title">Who we are</h5>
           </div>
         </header>
         <ClientContent />
--- pages/locations.js
+++ pages/locations.js
@@ -19,63 +19,63 @@ import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
 export default function Locations () {
     return (
       <Layout>
-        <header className="d-flex justify-content-center align-items-center" id="location-header">
+        <header className="align-items-origin" id="location-header">
           <div>
-            <h2 className="main-title text-center">
+            <h2 className="primary-title">
               <b>Our Locations</b>
             </h2>
-            <h5 className="secondary-title text-center">Find us at our nearest locations</h5>
+            <h5 className="secondary-title">Find us at our nearest locations</h5>
           </div>
         </header>
         <Row className="section-wrapper location-section">
-          <Col className="d-flex justify-content-center align-items-center">
+          <Col xs={12} sm={12} md={12} lg={6} xl={6} className="align-items-origin">
             <Card className="location-card" variant="outlined">
               <CardContent>
                 <CardMedia allowfullscreen="true" alt="Map of our location in Lagos" component="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.5678452143097!2d3.4442845147406214!3d6.449486795334054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf4ef6230b7f7%3A0xe940b1a33f163690!2s15%20Ojomu%20Rd%2C%20Ikoyi%2C%20Lagos!5e0!3m2!1sen!2sng!4v1620729370516!5m2!1sen!2sng" title="Location in Abuja" width="650" height="300" className="location-map" loading="lazy" />
               </CardContent>
               <hr className="line-width" />
-              <CardContent className="d-flex align-items-center justify-content-center">
+              <CardContent className="align-items-origin">
                 <div className="location-card-text-layout">
                   <h1 className="card-title">
                     {" "}
                     <b>Lagos</b>
                   </h1>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="https://goo.gl/maps/yKLv5axFieKFhcC6A">
                       <a className="body-link">
-                        <LocationOnIcon /> 15BB, Ojomu Road (Formerly Ruxton), Ikoyi, Lagos, Nigeria
+                        <LocationOnIcon /> 15BB, Ojomu (FMLY. Ruxton) Rd., Ikoyi, Lagos, Nigeria
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="/contact-us">
                       <a className="body-link">
                         <MailOutlineIcon /> info@expeditemoversng.com
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="/contact-us">
                       <a className="body-link">
                         <AlternateEmailIcon /> expeditemovers@gmail.com
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="tel:+2348103874982">
                       <a className="body-link">
                         <PhoneIcon /> +234 8103874982
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="tel:+2348179384101">
                       <a className="body-link">
                         <PhoneOutlinedIcon /> +234 8179384101
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="tel:012914338">
                       <a className="body-link">
                         <PhoneOutlinedIcon /> 01-2914338
@@ -86,53 +86,51 @@ export default function Locations () {
               </CardContent>
             </Card>
           </Col>
-          <Col className="d-flex justify-content-center align-items-center">
+          <Col xs={12} sm={12} md={12} lg={6} xl={6} className="align-items-origin">
             <Card className="location-card" variant="outlined">
               <CardContent>
                 <CardMedia component="iframe" alt="Map of our location in Abuja" title="Location in Abuja" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.8934100361953!2d7.440046214757513!3d9.07347449349005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0ad43bcc84fd%3A0x3520b427f27b6040!2s29%20Anthony%20Enahoro%20St%2C%20Mabushi%2C%20Abuja!5e0!3m2!1sen!2sng!4v1620727881038!5m2!1sen!2sng" width="650" height="300" className="location-map" allowfullscreen="true" loading="lazy" />
               </CardContent>
               <hr className="line-width" />
-              <CardContent className="d-flex align-items-center justify-content-center">
+              <CardContent className="align-items-origin">
                 <div className="location-card-text-layout">
-                  <h1 className="card-title">
-                    Abuja
-                  </h1>
-                  <div className="pb-2">
+                  <h1 className="card-title">Abuja</h1>
+                  <div className="location-card-text-container">
                     <Link href="https://goo.gl/maps/EhP4cgRquqGw1gMo9">
                       <a className="body-link">
                         <LocationOnIcon /> 29, Anthony Enahoro Street, Utako, Abuja, Nigeria
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="/contact-us">
                       <a className="body-link">
                         <MailOutlineIcon /> info@expeditemoversng.com
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="/contact-us">
                       <a className="body-link">
                         <AlternateEmailIcon /> expeditemovers@gmail.com
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="tel:+2348103874982">
                       <a className="body-link">
                         <PhoneIcon /> +234 8103874982
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="tel:+2348179384101">
                       <a className="body-link">
                         <PhoneOutlinedIcon /> +234 8179384101
                       </a>
                     </Link>
                   </div>
-                  <div className="pb-2">
+                  <div className="location-card-text-container">
                     <Link href="tel:012914338">
                       <a className="body-link">
                         <PhoneOutlinedIcon /> 01-2914338
